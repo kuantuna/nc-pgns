0731b086428168981799801a7467315ffbf7acf5
Tuna Tuncer <tuna.tuncer@ozu.edu.tr>
2023-04-14 17:48:13 +0300

    divided the pgns

---
+++ divide-pgns.py
@@ -0,0 +1,21 @@
+import chess
+import chess.pgn
+
+pgn_path = "2020/2500+.pgn"
+
+pgn = open(pgn_path)
+
+game_counter = 0
+start_idx = 1
+end_idx = 50
+while True:
+    game: chess.pgn.Game = chess.pgn.read_game(pgn)
+    if game is None:
+        break
+    print(game, file=open(
+        f"2020/2500+/{start_idx}-{end_idx}.pgn", "a"), end="\n\n")
+    game_counter += 1
+    if game_counter == 50:
+        start_idx += 50
+        end_idx += 50
+        game_counter = 0
